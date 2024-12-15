220e9ddd34ba5b11c3984861245a8733be3f55ec
Kyla <kflmorcillos@gmail.com>
2024-12-15 23:12:14 +0800

    Add java code

---
+++ TestProduct.java
@@ -0,0 +1,15 @@
+/**
+ * LABORATORY EXERCISE #3.b
+ * Display the product of A and B without declaring another variable.
+ * Display the result in this manner: 2 * 4 = 8
+ */
+
+public class TestProduct {
+	public static void main(String[] args) {
+		int A = 2, B = 4;
+		
+		System.out.print(A + " * " + B + " = ");
+		System.out.print(A * B);
+		
+	}
+}
